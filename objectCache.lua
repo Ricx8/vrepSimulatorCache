5115a982832f824d49d25b04f617e3d77c796b40
Ricardo <marquessricardo@gmail.com>
2018-05-22 01:33:19 -0400

    Working with a list of joints (cacheSimulation.txt only)

--- objectCache.lua
+++ objectCache.lua
@@ -13,33 +13,9 @@ end
 function sysCall_init()
     -- do some initialization here:
 
-    -- Make sure you read the section on "Accessing general-type objects programmatically"
-    -- For instance, if you wish to retrieve the handle of a scene object, use following instruction:
-    --
-    -- handle=sim.getObjectHandle('sceneObjectName')
-    --
-    -- Above instruction retrieves the handle of 'sceneObjectName' if this script's name has no '#' in it
-    --
-    -- If this script's name contains a '#' (e.g. 'someName#4'), then above instruction retrieves the handle of object 'sceneObjectName#4'
-    -- This mechanism of handle retrieval is very convenient, since you don't need to adjust any code when a model is duplicated!
-    -- So if the script's name (or rather the name of the object associated with this script) is:
-    --
-    -- 'someName', then the handle of 'sceneObjectName' is retrieved
-    -- 'someName#0', then the handle of 'sceneObjectName#0' is retrieved
-    -- 'someName#1', then the handle of 'sceneObjectName#1' is retrieved
-    -- ...
-    --
-    -- If you always want to retrieve the same object's handle, no matter what, specify its full name, including a '#':
-    --
-    -- handle=sim.getObjectHandle('sceneObjectName#') always retrieves the handle of object 'sceneObjectName'
-    -- handle=sim.getObjectHandle('sceneObjectName#0') always retrieves the handle of object 'sceneObjectName#0'
-    -- handle=sim.getObjectHandle('sceneObjectName#1') always retrieves the handle of object 'sceneObjectName#1'
-    -- ...
-    --
-    -- Refer also to sim.getCollisionhandle, sim.getDistanceHandle, sim.getIkGroupHandle, etc.
-
     fileName = "cacheSimulation.txt"
-    oldTime = 0
+    listOfjoints = {"backLLeg_Joint01", "backLLeg_Joint02", "backLLeg_Joint03"}
+    handleList = {}
 
     -- Open open a file in write mode
     outFile = io.open(fileName, "w")
@@ -47,8 +23,10 @@ function sysCall_init()
     --io.write("INIT\n")
 
     -- Get joints handles
-    handle = sim.getObjectHandle("backLLeg_Joint01")
-    --io.write(handle .. "\n")
+    for count=1, #listOfjoints, 1 do
+      handleList[count] = sim.getObjectHandle(listOfjoints[count])
+    end
+    --handle = sim.getObjectHandle("backLLeg_Joint01")
 end
 
 function sysCall_actuation()
@@ -64,11 +42,17 @@ end
 function sysCall_sensing()
     -- put your sensing code here
     currentTime = sim.getSimulationTime() -- Get current simulation time
+    local outLine = currentTime
 
     -- Get joints angles
-    jointAngle = sim.getJointPosition(handle)
+    --jointAngle = sim.getJointPosition(handle)
+    for count=1, #listOfjoints, 1 do
+      local joint = sim.getJointPosition(handleList[count])
+      outLine = outLine..";"..joint
+    end
 
-    io.write(currentTime .. ";" .. jointAngle .. "\n")
+    --io.write(currentTime .. ";" .. jointAngle .. "\n")
+    io.write(outLine.."\n")
 end
 
 function sysCall_cleanup()
